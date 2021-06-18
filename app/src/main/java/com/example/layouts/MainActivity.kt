cacb0bed855080a19628e6f5a40e8ca5d487aa94
Delong90 <jeka0307@mail.ru>
2021-06-18 17:02:06 +0300

    init

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.layouts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import com.example.layouts.databinding.SwipeRefreshLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var random: Random
+    private lateinit var handler: Handler
+    private lateinit var runnable: Runnable
+    private lateinit var binding: SwipeRefreshLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = SwipeRefreshLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        random = Random()
+        handler = Handler()
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            // Initialize a new Runnable
+            runnable = Runnable {
+                // Update the text view text with a random number
+                binding.textView.text = "Котика пора кормить. Его не кормили уже ${1 + random.nextInt(10)} минут"
+
+                // Hide swipe to refresh icon animation
+                binding.swipeRefreshLayout.isRefreshing = false
+            }
+
+            // Execute the task after specified time
+            handler.postDelayed(
+                runnable, 3000.toLong()
+            )
+        }
+
+        binding.swipeRefreshLayout.setColorSchemeResources(
+            android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light
+        )
     }
 }
